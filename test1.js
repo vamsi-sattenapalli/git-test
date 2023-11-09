f2249d986afb8cffa68b76ebd86624de2d3bc884
Vamsi71210 <111362370+Vamsi71210@users.noreply.github.com>
2023-11-09 13:11:25 -0500

    First Commit

---
+++ test1.js
@@ -0,0 +1,16 @@
+var a=1;
+var b=2;
+var c=a+b;
+var d={};
+d.fname='vamsi';
+d.lname='sattenapalli';
+d.age=26;
+console.log(c)
+console.log(d.fname)
+result=a/b;
+if(result%2==0){
+    console.log("even")
+}
+else if(result%2!=0){
+    console.log("odd")
+}
